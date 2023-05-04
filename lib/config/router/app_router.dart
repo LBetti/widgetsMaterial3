bd8e74386bc69dc93bab4ce49bcb16a2e3778f4a
Luciano <lucianobetti24@gmail.com>
2023-05-04 19:35:28 -0300

    routes with name

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,15 +4,18 @@ import '../../presentation/screens/screens.dart';
 
 final appRouter = GoRouter(routes: [
   GoRoute(
+    name: HomeScreen.name,
     path: '/',
     builder: (context, state) => const HomeScreen(),
   ),
   GoRoute(
     path: '/buttons',
+    name: ButtonsScreen.name,
     builder: (context, state) => const ButtonsScreen(),
   ),
   GoRoute(
     path: '/cards',
+    name: CardsScreen.name,
     builder: (context, state) => const CardsScreen(),
   ),
 ]);
