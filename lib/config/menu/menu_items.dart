dd47c23bf9d0bea5a65c7bbc9f309c9653d41ba1
Luciano <lucianobetti24@gmail.com>
2023-05-08 10:17:06 -0300

    progress indicators

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,9 @@ const appMenuItems = <MenuItem>[
       subTtitle: 'Contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  MenuItem(
+      title: 'ProgressIndicator',
+      subTtitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
 ];
