904cbaf68de24a1abd8a0534e07be441180a287f
Luciano <lucianobetti24@gmail.com>
2023-05-09 17:15:59 -0300

    images added

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -44,4 +44,9 @@ const appMenuItems = <MenuItem>[
       subTtitle: 'Serie de controles de Flutter',
       link: '/ui-controls',
       icon: Icons.car_rental_outlined),
+  MenuItem(
+      title: 'Introducción a la aplicación',
+      subTtitle: 'Tutorial introductorio',
+      link: '/tutorial',
+      icon: Icons.accessible_rounded),
 ];
